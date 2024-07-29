3d61d41267b31c8f726fc12de8d5354b95cf42b7
Data and Knowledge Engineering Lab, Kyung-Hee University <88026361+dke-lab@users.noreply.github.com>
2024-07-29 14:07:38 +0900

    Initial Commit
    dgll baseline

---
+++ dgll/data/dgraph.py
@@ -0,0 +1,111 @@
+"""
+This `DGraph` class represents a graph data structure. Here's a summary of what each method does:
+
+- `__init__`: Initializes the graph with nodes, edges, labels, features, and masks for training, testing, and validation subsets.
+- `get_neighbors`: Returns the neighbors of a list of nodes.
+- `get_labels`: Returns the labels associated with a list of nodes.
+- `get_features`: Returns the features associated with a list of nodes.
+- `get_train_nodes`: Returns the nodes in the training subset.
+- `get_validation_nodes`: Returns the nodes in the validation subset.
+- `get_test_nodes`: Returns the nodes in the testing subset.
+
+Note that the `get_neighbors` method assumes that the `edges` attribute is a list where each element is a list of neighbor indices for the corresponding node.
+"""
+
+
+class DGraph(object):
+    def __init__(
+        self,
+        nodes=None,
+        edges=None,
+        labels=None,
+        features=None,
+        train_mask=None,
+        test_mask=None,
+        validation_mask=None,
+    ):
+        """
+        Initializes a DGraph object with the given parameters.
+
+        Args:
+            nodes (Optional[List[int]]): A list of node indices.
+            edges (Optional[List[List[int]]]): A list of lists representing the edges in the graph.
+            labels (Optional[List[int]]): A list of labels associated with each node.
+            features (Optional[List[List[float]]]): A list of features associated with each node.
+            train_mask (Optional[List[bool]]): A list of booleans indicating whether a node is in the training subset.
+            test_mask (Optional[List[bool]]): A list of booleans indicating whether a node is in the testing subset.
+            validation_mask (Optional[List[bool]]): A list of booleans indicating whether a node is in the validation subset.
+        """
+        self.nodes = nodes
+        self.edges = edges
+        self.labels = labels
+        self.features = features
+        self.train_mask = train_mask
+        self.test_mask = test_mask
+        self.validation_mask = validation_mask
+
+    def get_neighbors(self, nodes):
+        """
+        Get the neighbors of the given nodes.
+
+        Parameters:
+            nodes (tensor[int]): A tensor of node indices.
+
+        Returns:
+            List[List[int]]: A list of lists containing the neighbors of each node.
+        """
+        result = []
+        for node in nodes:
+            result.append(self.edges[node.item()])
+        return result
+
+    def get_labels(self, nodes):
+        """
+        Get the labels associated with the given nodes.
+
+        Parameters:
+            nodes (tensor[int]): A tensor of node indices.
+
+        Returns:
+            tensor[int]: A tensor of labels corresponding to the given node indices.
+        """
+        return self.labels[nodes]
+
+    def get_features(self, nodes):
+        """
+        Get the features associated with the given nodes.
+
+        Parameters:
+            nodes (tensor[int]): A tensor of node indices.
+
+        Returns:
+            tensor[float]: A tensor of features corresponding to the given node indices.
+        """
+        return self.features[nodes]
+
+    def get_train_nodes(self):
+        """
+        Get the indices of nodes that are in the training subset.
+
+        Returns:
+            tensor[int]: A tensor containing the indices of nodes in the training subset.
+        """
+        return self.nodes[self.train_mask]
+
+    def get_validation_nodes(self):
+        """
+        Get the indices of nodes that are in the validation subset.
+
+        Returns:
+            tensor[int]: A tensor containing the indices of nodes in the validation subset.
+        """
+        return self.nodes[self.validation_mask]
+
+    def get_test_nodes(self):
+        """
+        Get the indices of nodes that are in the test subset.
+
+        Returns:
+            tensor[int]: A tensor containing the indices of nodes in the test subset.
+        """
+        return self.nodes[self.test_mask]
---
+++ example.py
@@ -0,0 +1,57 @@
+import pickle
+
+"""
+NOTE: VERY IMPORTANT
+--------------------
+Do not use "import torch"
+Instead, use "from dgll import backend as F"
+In your code, use "F.tensor" or any PyTorch functions using "F."
+PyTorch is already included as a backend in the dgll library to ensure consistency and the possibily of multiple backends.
+"""
+from dgll import backend as F
+
+# Load the graph
+# You can download cora.graph and products.graph from the server
+g = pickle.load(open("cora.graph", "rb"))
+
+# A set of node IDs
+nodes = F.tensor([0, 5, 6, 9, 23])
+
+# Get the neighbors of the nodes
+neighbors = g.get_neighbors(nodes)
+
+# Get the labels of the nodes
+labels = g.get_labels(nodes)
+
+# Get the features of the nodes
+features = g.get_features(nodes)
+
+# Get the nodes that are in the training subset
+train_nodes = g.get_train_nodes()
+
+# Get the nodes that are in the validation subset
+validation_nodes = g.get_validation_nodes()
+
+# Get the nodes that are in the test subset
+test_nodes = g.get_test_nodes()
+
+# Print the results
+print(neighbors)
+print(labels)
+print(features)
+print(train_nodes)
+print(validation_nodes)
+print(test_nodes)
+
+
+# Output
+# [[809, 1217, 1218], [2, 114, 516, 541], [88, 91, 200, 226, 728], [2, 89, 121, 182, 208], [505, 582]]
+# tensor([1, 4, 6, 4, 4])
+# tensor([[0, 0, 0,  ..., 0, 0, 0],
+#         [0, 0, 0,  ..., 0, 0, 0],
+#         [0, 0, 0,  ..., 0, 0, 0],
+#         [0, 0, 0,  ..., 0, 0, 0],
+#         [0, 0, 0,  ..., 0, 0, 0]], dtype=torch.int32)
+# tensor([   0,    9,   10,  ..., 2701, 2704, 2706])
+# tensor([   2,    7,    8,  ..., 2697, 2702, 2707])
+# tensor([   1,    3,    4,  ..., 2700, 2703, 2705])
